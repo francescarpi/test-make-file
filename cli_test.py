589ea13e9eaa2ca974684cdc6233bfea2bd676f3
francesc.arpi@gmail.com <137821137821>
2019-04-25 18:25:14 +0200

    New test

--- cli_test.py
+++ cli_test.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 import json
 import sys
+import time
 
 
 class Client:
@@ -33,6 +34,21 @@ class Client:
     def test_action_5(self):
         response = {'status': 'success_at_retry_{}'.format(self._json['common']['retries'])}
         return json.dumps(response)
+
+    def test_action_6(self):
+        sys.stdout.write("Stdout 1: Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.\n\n")
+        sys.stdout.flush()
+        time.sleep(2)
+        sys.stdout.write("Stdout 2: Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.\n\n")
+        sys.stdout.flush()
+        time.sleep(2)
+        sys.stderr.write("Stderr 1: Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.\n\n")
+        sys.stderr.flush()
+        time.sleep(2)
+        sys.stderr.write("Stderr 2: Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.\n\n")
+        sys.stderr.flush()
+
+        return 'Finish'
 
 
 if __name__ == '__main__':
